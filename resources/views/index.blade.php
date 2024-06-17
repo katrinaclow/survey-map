fa477f412177d7ddc705e39f38b3371c521371e3
katrinaclow <contact@katrinaclow.com>
2024-06-17 18:17:36 -0300

    page skeleton created

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Locus Surveys</title>
+</head>
+<body>
+    <h1>Locus Surveys Home Page</h1>
+</body>
+</html>
