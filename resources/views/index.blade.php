112ba36b73c8cd40a70acb571721c279bde43bab
katrinaclow <contact@katrinaclow.com>
2024-06-19 11:46:37 -0300

    page outlines created

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Locus Surveys</title>
-</head>
-<body>
-    <h1>Locus Surveys Home Page</h1>
-</body>
-</html>
+<x-layout>
+    <x-slot:heading>
+        Home Page
+    </x-slot:heading>
+    <h1>Home Page Content</h1>
+</x-layout>
---
+++ resources/views/map.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        Map Page
+    </x-slot:heading>
+    <h1>Map Page Content</h1>
+</x-layout>
